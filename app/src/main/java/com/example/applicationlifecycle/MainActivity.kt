71d4c22c2136ebcac1fbcf66d7be10b02df0ea5d
Andrii Krenevych <krenevych@knu.ua>
2024-09-25 22:00:23 +0300

    log -> timber

--- app/src/main/java/com/example/applicationlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/applicationlifecycle/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.applicationlifecycle
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
